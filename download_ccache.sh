e055cc0fcf87333af40907543cc3597252268b8a
virus9281 <72541771+virus9281@users.noreply.github.com>
2021-06-18 22:50:50 +0530

    Update download_ccache.sh

--- download_ccache.sh
+++ download_ccache.sh
@@ -6,8 +6,7 @@ ccache_download ()
 {
   mkdir -p ~/.config/rclone
   echo "$rclone_config" > ~/.config/rclone/rclone.conf
-  rclone copy drive:ccache/waveOS/ccache.tar.zst /tmp -P
-  tar -xaf ccache.tar.zst
+  rclone copy drive:ccache/waveOS/ccache.tar.zst /tmp -P && tar -xaf ccache.tar.zst
   tar "-I zstd -1 -T8" -xf ccache.tar.zst
   rm -rf ccache.tar.zst
 }
